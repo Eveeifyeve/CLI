ba1d6e7ae77709b73bed439b2e1757059395888b
eveeify <eveeg1971@gmail.com>
2024-02-14 04:23:12 +1100

    fix: install script to curl

--- install.sh
+++ install.sh
@@ -1,2 +1,7 @@
-cp ./src/eve.sh /usr/local/bin/eve
+#!/bin/bash
+
+# Download the eve.sh script from the given URL
+curl -o /usr/local/bin/eve https://raw.githubusercontent.com/Eveeifyeve/cli/Stable/src/eve.sh
+
+# Make the downloaded script executable
 chmod +x /usr/local/bin/eve
