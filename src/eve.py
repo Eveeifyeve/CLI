8b8d2384b46b77895f253dc2799dd1e8bef8b574
eveeify <eveeg1971@gmail.com>
2024-02-14 23:39:55 +1100

    feat: uninstall command

--- src/eve.py
+++ src/eve.py
@@ -9,7 +9,7 @@ def cli():
 @cli.command(name='git_checkFolders')
 @click.argument('root_folder', type=click.Path(exists=True))
 def git_checkfolders(root_folder):
-    """Check if subfolders of ROOT_FOLDER are Git repositories and if they are committed."""
+    """Check if subfolders of FOLDER are committed."""
     for dirpath, dirnames, _ in os.walk(root_folder):
         for dirname in dirnames:
             folder = os.path.join(dirpath, dirname)
@@ -22,6 +22,96 @@ def git_checkfolders(root_folder):
                     pass 
             else:
                 pass 
+            
+
+def read_apps_from_file(filename):
+    with open(filename, 'r') as file:
+        return [line.strip() for line in file]
+    
+# Manually split the APPS list into formulas and casks
+formulas = [
+    'cmake',
+    'gh',
+    'gradle',
+    'jenv',
+    'neovim',
+    'spicetify-cli',
+    'tmux'
+]
+
+casks = [
+    'wezterm',
+    'neovide',
+    'raycast',
+]
+
+
+def is_cask(formula_name):
+    try:
+        # List all casks
+        casks = subprocess.check_output(['brew', 'list', '--casks'], text=True).splitlines()
+        return formula_name in casks
+    except subprocess.CalledProcessError:
+        return False
+
+
+@click.group()
+def reinstall():
+    """Reinstall apps from Homebrew."""
+    pass
+
+@reinstall.command(name='formulas')
+def reinstall_formulas():
+    """Reinstall all Homebrew formulas."""
+    for formula_name in formulas:
+        if not is_cask(formula_name):
+            reinstall_app(formula_name)
+
+@reinstall.command(name='casks')
+def reinstall_casks():
+    """Reinstall all Homebrew casks."""
+    for formula_name in casks:
+        if is_cask(formula_name):
+            reinstall_app(formula_name)
+
+
+def reinstall_app(formula_name):
+    try:
+        # Update Homebrew
+        subprocess.check_call(['brew', 'update'])
+        
+        # Check if the symlink already exists and remove it if necessary
+        symlink_target = f"/opt/homebrew/bin/{formula_name}"
+        if os.path.exists(symlink_target):
+            os.remove(symlink_target)
+        
+        # Determine if the item is a cask
+        is_cask_item = is_cask(formula_name)
+        
+        # Unlink the app or cask
+        if not is_cask_item:
+            try:
+                subprocess.check_call(['brew', 'unlink', formula_name])
+            except subprocess.CalledProcessError:
+                pass
+        
+        # Reinstall the app or cask
+        if is_cask_item:
+            subprocess.check_call(['brew', 'install', '--cask', formula_name])
+        else:
+            subprocess.check_call(['brew', 'install', formula_name])
+        
+        # Link the app with --overwrite flag
+        try:
+            subprocess.check_call(['brew', 'link', '--overwrite', formula_name])
+        except subprocess.CalledProcessError:
+            pass
+    except subprocess.CalledProcessError:
+        pass
+
+cli.add_command(reinstall)
+
 
 if __name__ == '__main__':
     cli()
+
