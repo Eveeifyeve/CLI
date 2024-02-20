8b4c425a0a9bb9f47004da302d220c6d9518618b
Eveeifyeve <eveeg1971@gmail.com>
2024-02-21 02:08:40 +1100

    chore: moved to typer from click

--- src/eve.py
+++ src/eve.py
@@ -1,16 +1,11 @@
-import click
+import typer
 import os
 import subprocess
 
+app = typer.Typer()
 
-@click.group()
-def cli():
-    pass
-
-
-@cli.command(name="git_checkFolders")
-@click.argument("root_folder", type=click.Path(exists=True))
-def git_checkfolders(root_folder):
+@app.command()
+def git_checkfolders(root_folder: str):
     """Check if subfolders of FOLDER are committed."""
     for dirpath, dirnames, _ in os.walk(root_folder):
         for dirname in dirnames:
@@ -21,19 +16,16 @@ def git_checkfolders(root_folder):
                         ["git", "-C", folder, "status"], capture_output=True, text=True
                     )
                     if "nothing to commit" not in result.stdout:
-                        click.echo(f"Uncommitted changes detected at: {folder}")
+                        typer.echo(f"Uncommitted changes detected at: {folder}")
                 except Exception as e:
                     pass
             else:
                 pass
 
-
-def read_apps_from_file(filename):
+def read_apps_from_file(filename: str) -> list:
     with open(filename, "r") as file:
         return [line.strip() for line in file]
 
-
-# Manually split the APPS list into formulas and casks
 formulas = ["cmake", "gh", "gradle", "jenv", "neovim", "spicetify-cli", "tmux"]
 
 casks = [
@@ -42,10 +34,8 @@ casks = [
     "raycast",
 ]
 
-
-def is_cask(formula_name):
+def is_cask(formula_name: str) -> bool:
     try:
-        # List all casks
         casks = subprocess.check_output(
             ["brew", "list", "--casks"], text=True
         ).splitlines()
@@ -53,56 +43,43 @@ def is_cask(formula_name):
     except subprocess.CalledProcessError:
         return False
 
+reinstall_app = typer.Typer()
 
-@click.group()
-def reinstall():
-    """Reinstall apps from Homebrew."""
-    pass
-
-
-@reinstall.command(name="formulas")
-def reinstall_formulas():
+@reinstall_app.command()
+def formulas():
     """Reinstall all Homebrew formulas."""
     for formula_name in formulas:
         if not is_cask(formula_name):
-            reinstall_app(formula_name)
+            reinstall_formula(formula_name)
 
-
-@reinstall.command(name="casks")
-def reinstall_casks():
+@reinstall_app.command()
+def casks():
     """Reinstall all Homebrew casks."""
     for formula_name in casks:
         if is_cask(formula_name):
-            reinstall_app(formula_name)
+            reinstall_formula(formula_name)
 
-
-def reinstall_app(formula_name):
+def reinstall_formula(formula_name: str):
     try:
-        # Update Homebrew
         subprocess.check_call(["brew", "update"])
 
-        # Check if the symlink already exists and remove it if necessary
         symlink_target = f"/opt/homebrew/bin/{formula_name}"
         if os.path.exists(symlink_target):
             os.remove(symlink_target)
 
-        # Determine if the item is a cask
         is_cask_item = is_cask(formula_name)
 
-        # Unlink the app or cask
         if not is_cask_item:
             try:
                 subprocess.check_call(["brew", "unlink", formula_name])
             except subprocess.CalledProcessError:
                 pass
 
-        # Reinstall the app or cask
         if is_cask_item:
             subprocess.check_call(["brew", "install", "--cask", formula_name])
         else:
             subprocess.check_call(["brew", "install", formula_name])
 
-        # Link the app with --overwrite flag
         try:
             subprocess.check_call(["brew", "link", "--overwrite", formula_name])
         except subprocess.CalledProcessError:
@@ -110,24 +87,17 @@ def reinstall_app(formula_name):
     except subprocess.CalledProcessError:
         pass
 
+app.add_typer(reinstall_app, name="reinstall")
 
-cli.add_command(reinstall)
-
-
-@cli.command(name="link_folders")
-@click.argument("root_folder", type=click.Path(exists=True))
-@click.option(
-    "--target", default="~/links", help="Target directory for symbolic links."
-)
-def link_folders(root_folder, target):
+@app.command()
+def link_folders(root_folder: str, target: str = typer.Option(default="~/links", help="Target directory for symbolic links.")):
     """Create a symbolic link from ROOT_FOLDER to TARGET."""
     target = os.path.expanduser(target)
     if not os.path.exists(target):
         subprocess.run(["ln", "-s", root_folder, target], check=True)
-        click.echo(f"Created link: {target} -> {root_folder}")
+        typer.echo(f"Created link: {target} -> {root_folder}")
     else:
-        click.echo(f"Link already exists: {target}")
-
+        typer.echo(f"Link already exists: {target}")
 
 if __name__ == "__main__":
-    cli()
+    app()
