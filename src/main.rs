f4f45bc46cb5a60416def0725c800a8ec8e90be9
Eveeifyeve <eveeg1971@gmail.com>
2024-02-23 08:52:35 +1100

    feat: rust init

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+#[allow(dead_code)]
+mod ascii;
+mod gitcheck;
+mod reinstall;
+
+use std::path::PathBuf;
+
+use ascii::{CYAN, YELLOW};
+use clap::{arg, Command};
+
+fn cli() -> Command {
+    Command::new("eve")
+    .about("A CLI Made for Eveeifyeve")
+    .version("1.0.1")
+    .subcommand_required(true)
+    .arg_required_else_help(true)
+    .allow_external_subcommands(true)
+    .subcommand(
+        Command::new("Check_commits")
+        .about("Checks if you have commited in folders")
+        .arg(arg!(<PATH> "").value_parser(clap::value_parser!(PathBuf)))
+    )
+    .help_template(format!("{}{{name}} {{version}}\n{{author-with-newline}}{{about-with-newline}}\n{{usage-heading}} {{usage}}\n\n {}Commands: \n{{subcommands}}\n {}Options: \n{{options}}", CYAN, YELLOW, YELLOW))
+}
+
+fn main() {
+    let commands = cli().get_matches();
+
+    match commands.subcommand() {
+        Some(("Check_commits", sub_match)) => {
+            if let Some(path) = sub_match.get_one::<PathBuf>("PATH") {
+                gitcheck::main(path)
+            }
+        }
+        Some(("Reinstall", sub_match)) => {
+            reinstall::main();
+        }
+
+        _ => unreachable!(),
+    }
+}
